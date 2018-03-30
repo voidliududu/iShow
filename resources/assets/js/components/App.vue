95be419880fbaf87423647977980ef0e5cef1c27
voidliududu <3037565218@qq.com>
2018-03-30 21:17:45 +0800

    初始化仓库

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                <div class="layout-nav">
+                  <MenuItem name="1"><icon></icon>item1</MenuItem>
+                  <MenuItem name="1"><icon></icon>item1</MenuItem>
+                  <MenuItem name="1"><icon></icon>item1</MenuItem>
+                </div>
+              </Menu>
+
+            </Header>
+        </Layout>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "app"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta charset="utf-8" />
+    <title>test</title>
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body>
+<div id="app">
+    <App/>
+</div>
+</body>
+</html>
